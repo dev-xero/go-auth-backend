6076df981821a58d1c25f2c441e8bdb8d2986bc5
dev-xero <elo48858@gmail.com>
2024-02-04 07:46:27 +0100

    feat: Add environment variables

--- application/app.go
+++ application/app.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type App struct{}
@@ -18,12 +21,19 @@ func baseHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *App) Start(ctx context.Context) error {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("[FAIL]: unable to load environment variables")
+	}
+
+	var port string = os.Getenv("PORT")
+
 	server := &http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf(":%s", port),
 		Handler: http.HandlerFunc(baseHandler),
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("[FAIL]: unable to start server")
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dev-xero/authentication-backend
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
