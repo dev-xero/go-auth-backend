3aa331c2ef6a5bf80b697102bc1acf008a344207
dev-xero <elo48858@gmail.com>
2024-02-05 20:23:04 +0100

    feat: Implement json web token creation and signing

---
+++ authentication/jwt.go
@@ -0,0 +1,38 @@
+package authentication
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+)
+
+func CreateJWToken(userID uuid.UUID) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return "", fmt.Errorf("[FAIL]: could not load environment variables: %w", err)
+	}
+
+	var secretKey = os.Getenv("JWT_SECRET_KEY")
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":    userID,
+		"issuer": "go-auth-server",
+		"aud":    "user",
+		"exp":    time.Now().Add(time.Hour).Unix(),
+		"iat":    time.Now().Unix(),
+	})
+
+	log.Printf("Token claims added: %+v\n", claims)
+
+	tokenString, err := claims.SignedString(secretKey)
+	if err != nil {
+		return "", fmt.Errorf("[FAIL]: could not sign token: %w", err)
+	}
+
+	return tokenString, nil
+}
--- go.mod
+++ go.mod
@@ -11,3 +11,5 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
 )
+
+require github.com/golang-jwt/jwt/v5 v5.2.0
