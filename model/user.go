660c3c68b003aa6f634d8296e07c62ddefcd16f7
dev-xero <elo48858@gmail.com>
2024-02-06 10:50:55 +0100

    chore: Add json struct tags to user model

--- model/user.go
+++ model/user.go
@@ -3,8 +3,8 @@ package model
 import "github.com/google/uuid"
 
 type User struct {
-	ID       uuid.UUID
-	Username string
-	Email    string
-	Password string
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
 }
