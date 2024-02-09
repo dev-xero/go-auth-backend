cb848d683f78210e0586c20c86e583f7b0feb0cf
dev-xero <elo48858@gmail.com>
2024-02-09 12:29:36 +0100

    docs: Add code documentation for email.go

--- util/email.go
+++ util/email.go
@@ -5,6 +5,19 @@ import (
 	"regexp"
 )
 
+/*
+Compares and verifies if a provided string is a valid email format
+
+Objectives:
+  - Create a valid email regex pattern
+  - Compare the provided string to the email regex pattern
+
+Params:
+  - email: A string to compare against the pattern
+
+Returns:
+  - True if the email string is a valid email pattern, false otherwise
+*/
 func IsValidEmail(email string) bool {
 	emailPattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 
