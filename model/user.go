2afcab0cd4b902bb6de9f1fdec357c6daf4c500b
dev-xero <elo48858@gmail.com>
2024-02-09 13:20:43 +0100

    docs: Add code documentation to user model source file

--- model/user.go
+++ model/user.go
@@ -2,6 +2,15 @@ package model
 
 import "github.com/google/uuid"
 
+/*
+User model struct
+
+Fields:
+  - ID:       uuid
+  - Username: string
+  - Email:    string
+  - Password: string
+*/
 type User struct {
 	ID       uuid.UUID `json:"id"`
 	Username string    `json:"username"`
