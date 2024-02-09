b131d66f28b6140b4725a1e874426628e0be105c
dev-xero <elo48858@gmail.com>
2024-02-09 12:33:10 +0100

    docs: Add code documentation to error.go

--- util/error.go
+++ util/error.go
@@ -7,14 +7,27 @@ import (
 
 type ValidationError error
 
-// Store possible validation errors that could arise
+// Stores all possible errors that may occur during input validation/
 var (
 	ErrEmptyFields    ValidationError = errors.New("one or more fields are empty")
 	ErrEmailInvalid   ValidationError = errors.New("invalid email provided")
 	ErrPasswordLength ValidationError = errors.New("password length must be at least 8 characters long")
 )
 
+/*
+Returns a custom error format
+
+Objectives:
+  - Prefix the error with an error message
+
+Params:
+  - err: The error to format
+  - msg: The string to prefix the error with
+
+Returns:
+  - A 64-bit signed integer, 0
+  - A properly formatted error
+*/
 func Fail(err error, msg string) (int64, error) {
-	// Custom error logger
 	return 0, fmt.Errorf("%s: %v", msg, err)
 }
