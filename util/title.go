c2a720c6c84ae257ef86f6f7c3f8e9fa0fbeaea4
dev-xero <elo48858@gmail.com>
2024-02-09 12:56:19 +0100

    docs: Add code documentation to title.go

--- util/title.go
+++ util/title.go
@@ -2,6 +2,19 @@ package util
 
 import "strings"
 
+/*
+Capitalizes the first letter of a string
+
+Objectives:
+  - Return the string if its a single letter
+  - Return the same string with the first letter capitalized
+
+Params:
+  - s: The string to capitalize
+
+Returns:
+  - The capitalized string
+*/
 func CapitalizeFirstLetter(s string) string {
 	if s == "" {
 		return s
