568d90dfb27b9432868f2475e4029de70936b1f0
dev-xero <elo48858@gmail.com>
2024-02-09 12:39:35 +0100

    docs: Add code documentation to headers.go

--- util/headers.go
+++ util/headers.go
@@ -2,6 +2,18 @@ package util
 
 import "net/http"
 
+/*
+Sets the response headers content type to application/json
+
+Objectives:
+  - Set the response content type header to a json type
+
+Params:
+  - w: A http response writer
+
+Returns:
+  - No return value
+*/
 func SetJSONHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
