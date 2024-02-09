b072511642d31c7d0d65eaf18bafbab3d5358839
dev-xero <elo48858@gmail.com>
2024-02-09 12:51:09 +0100

    docs: Add code documentation to response.go

--- util/response.go
+++ util/response.go
@@ -8,18 +8,52 @@ import (
 	"github.com/google/uuid"
 )
 
+/*
+Response body struct
+
+Fields:
+  - Message: string
+  - Success: bool
+  - Payload: interface{} (any valid go data type)
+*/
 type Response struct {
 	Message string      `json:"message"`
 	Success bool        `json:"success"`
 	Payload interface{} `json:"payload"`
 }
 
+/*
+User payload struct
+
+Fields:
+  - ID: uuid
+  - Username: string
+  - Email: string
+*/
 type UserPayload struct {
 	ID       uuid.UUID `json:"id"`
 	Username string    `json:"username"`
 	Email    string    `json:"email"`
 }
 
+/*
+Sends a JSON response to the client with an optional payload
+
+Objectives:
+  - Set the response content headers to a json type
+  - Build the response object with the message and payload
+  - Encode the response object as a json object
+  - Write the header and response body with the json object
+
+Params:
+  - w:       A http response writer
+  - msg:     The response body message
+  - status:  The response status
+  - payload: An optional payload
+
+Returns:
+  - No return value
+*/
 func JsonResponse(w http.ResponseWriter, msg string, status int, payload interface{}) {
 	SetJSONHeaders(w)
 
